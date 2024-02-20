7391b582d10825a5fa7738d6aa077566f43144de
martin-creator <martinlubowa@outlook.com>
2024-02-20 16:33:36 +0300

    Added http methods

--- flask_blog_project/server.py
+++ flask_blog_project/server.py
@@ -25,6 +25,61 @@ def user_profile(username):
     """
     return f"User profile for {username}"
 
+@app.route('/articles', method=['GET'])
+def get_articles():
+    """
+    This function takes no arguments and returns a string.
+    """
+    return 'List of all articles'
+
+
+@app.route('/articles', method=['POST'])
+def create_article():
+    """
+    This function takes no arguments and returns a string.
+    """
+    return 'Create a new article'
+
+@app.route('/articles/<int:article_id>', method=['GET'])
+def get_article(article_id):
+    """
+    This function takes an integer as an argument and returns a string.
+    """
+    return f'View article with id {article_id}'
+
+
+@app.route('/articles/<int:article_id>', method=['PUT'])
+def update_article(article_id):
+    """
+    This function takes an integer as an argument and returns a string.
+    """
+    return f'Update article with id {article_id}'
+
+
+@app.route('/articles/<int:article_id>', method=['DELETE'])
+def delete_article(article_id):
+    """
+    This function takes an integer as an argument and returns a string.
+    """
+    return f'Delete article with id {article_id}'
+
+@app.route('/articles/<int:article_id>/comments', method=['PATCH'])
+def update_comments(article_id):
+    """
+    This function takes an integer as an argument and returns a string.
+    """
+    return f'Update comments for article with id {article_id}'
+
+# Pass multiple http methods
+@app.route('/articles/<int:article_id>/comments', method=['GET', 'POST'])
+def get_or_create_comments(article_id):
+    """
+    This function takes an integer as an argument and returns a string.
+    """
+    return f'Get or create comments for article with id {article_id}'
+
+
+
 # # Generating a url for usr_profile
 # url = url_for('user_profile', username='michael')
 # print(url)
