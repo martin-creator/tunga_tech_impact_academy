8a3dbdcf040e532b9078ac050cd289f72b7c483a
martin-creator <martinlubowa@outlook.com>
2024-03-05 00:45:37 +0300

    Added data persitence using sql alchemy

---
+++ flask_blog_project/models/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class BlogPost(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Blog post {self.id}, {self.title}, {self.date_posted}"
--- flask_blog_project/server2.py
+++ flask_blog_project/server2.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField,TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
+from flask_sqlalchemy import SQLAlchemy
+from models import BlogPost,db
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
 app.config['SECRET_KEY'] = 'my_secret'
+
+db.init_app(app)
+
+# Create the database
+with app.app_context():
+    db.create_all()
 
 class BlogPostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(min=2, max=100)])
@@ -16,12 +26,20 @@ class myForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up'
+    submit = SubmitField('Sign Up', render_kw={"class":"btn btn-primary"})
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def create_post():
+    form = BlogPostForm()
+    
+    if form.validate_on_submit():
+        post = BlogPost()
+        form.populate_obj(post)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return '<h1>Post has been created!</h1>'
 
 
 @app.route('/blog', methods=['GET', 'POST'])
