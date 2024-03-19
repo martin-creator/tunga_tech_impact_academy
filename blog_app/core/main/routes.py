f0e6a18d1b9e4b64cdce7801d3ef02932d9c1c31
martin-creator <martinlubowa@outlook.com>
2024-03-19 17:34:37 +0300

    Added profile information

--- blog_app/core/main/routes.py
+++ blog_app/core/main/routes.py
@@ -1,7 +1,13 @@
 from core.main import bp
+from flask_login import login_required, current_user
 from flask import render_template
 
 
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+@bp.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
