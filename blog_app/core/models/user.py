042abdc01fbad3ba7400faeef3acac05ad13dfad
martin-creator <martinlubowa@outlook.com>
2024-03-19 15:49:47 +0300

    Created user model

---
+++ blog_app/core/models/user.py
@@ -0,0 +1,10 @@
+from core.extensions import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+    def __repr__(self):
+        return f'<User {self.name}>'
