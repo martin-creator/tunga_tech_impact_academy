b4618973baa10418add942a13c6eae03cf53c2db
martin-creator <martinlubowa@outlook.com>
2024-03-19 11:55:59 +0300

    Added blog core

--- blog_app/core/__init__.py
+++ blog_app/core/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from core.main import bp as main_bp
+from core.posts import bp as posts_bp
+from core.questions import bp as question_bp
+from core.extensions import db
 
 from config import Config
 
@@ -8,9 +11,12 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     # Initialize Flask extensions here
+    db.init_app(app)
 
     # Register blueprints here
     app.register_blueprint(main_bp)
+    app.register_blueprint(posts_bp, url_prefix='/posts')
+    app.register_blueprint(question_bp, url_prefix='/questions')
 
     @app.route('/test/')
     def test_page():
