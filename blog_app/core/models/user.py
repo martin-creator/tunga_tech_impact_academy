2e2b935829ddb330ac23163d2908d306b7041f7f
martin-creator <martinlubowa@outlook.com>
2024-03-22 12:31:00 +0300

    Added table name

--- blog_app/core/models/user.py
+++ blog_app/core/models/user.py
@@ -2,6 +2,9 @@ from flask_login import UserMixin
 from core.extensions import db
 
 class User(UserMixin, db.Model):
+
+    __tablename__ = 'user'
+    
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
