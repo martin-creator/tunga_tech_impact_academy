8a126793bacc669f4756b5e3897ef0faefa6180f
martin-creator <martinlubowa@outlook.com>
2024-03-24 23:27:34 +0300

    Created user bp

---
+++ blog_api/resources/user.py
@@ -0,0 +1,10 @@
+from flask.views import MethodView
+from flask_smorest import Blueprint, abort
+from passlib.hash import pbkdf2_sha256
+
+from db import db
+from models import UserModel
+from schemas import UserSchema
+
+
+blp = Blueprint("Users", "users", description="Operations on users")
