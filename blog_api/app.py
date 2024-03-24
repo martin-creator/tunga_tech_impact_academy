2de828b0c8b71f3313e39e2850c933447ce29033
martin-creator <martinlubowa@outlook.com>
2024-03-24 12:05:43 +0300

    Added dynamic endpoint

--- blog_api/app.py
+++ blog_api/app.py
@@ -17,3 +17,13 @@ def create_store():
     stores.append(new_store)
     return new_store, 201
 
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item
+    return {"message": "Store not found"}, 404
+
