8fe877f5b295a93c5516ce18c9ed83d9dd13e787
martin-creator <martinlubowa@outlook.com>
2024-03-24 22:48:24 +0300

    Added tag schemas

--- blog_api/schemas.py
+++ blog_api/schemas.py
@@ -11,6 +11,10 @@ class PlainStoreSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str()
 
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str()
+
 
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_only=True)
@@ -24,3 +28,8 @@ class ItemUpdateSchema(Schema):
 
 class StoreSchema(PlainStoreSchema):
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
+
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
