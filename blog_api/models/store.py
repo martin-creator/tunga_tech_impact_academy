c2ad80e981bfa21b135ca2e474781a859c234e9e
martin-creator <martinlubowa@outlook.com>
2024-03-24 22:37:47 +0300

    Added db perstistnace to store

--- blog_api/models/store.py
+++ blog_api/models/store.py
@@ -1,12 +1,13 @@
 from db import db
 
 
+
 class StoreModel(db.Model):
     __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete")
 
     # lazy="dynamic" is used to tell SQLAlchemy to not load the items automatically
     # This is useful when we have a large number of items in the database
