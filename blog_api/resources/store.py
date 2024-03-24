ff394cf3d14442c4dab2f4bb74543fb6f86e3772
martin-creator <martinlubowa@outlook.com>
2024-03-24 21:56:22 +0300

    Added status codes and schemas for items

--- blog_api/resources/store.py
+++ blog_api/resources/store.py
@@ -10,6 +10,7 @@ blp = Blueprint("stores", __name__, description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200, StoreSchema)
     def get(self, store_id):
         try:
             return stores[store_id]
@@ -26,10 +27,12 @@ class Store(MethodView):
 
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200, StoreSchema(many=True)) # many=True is used to return a list of stores
     def get(self):
         return {"stores": list(stores.values())}
 
     @blp.arguments(StoreSchema)
+    @blp.response(201, StoreSchema)
     def post(cls, store_data):
         for store in stores.values():
             if store_data["name"] == store["name"]:
