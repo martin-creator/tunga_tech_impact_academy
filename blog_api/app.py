fe82f9c58cac72d4ca7092a423091aca8fb54be5
martin-creator <martinlubowa@outlook.com>
2024-03-27 23:22:55 +0300

    Created Flask instance inside pattern

--- blog_api/app.py
+++ blog_api/app.py
@@ -2,25 +2,30 @@ from flask import Flask
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask import Flask, jsonify
-from db import db
+from blog_api.db import db
+from flask_migrate import Migrate
 import os
-from resources.user import blp as UserBlueprint
-from resources.item import blp as ItemBlueprint
-from resources.store import blp as StoreBlueprint
-from resources.tag import blp as TagBlueprint
+from blog_api.resources.user import blp as UserBlueprint
+from blog_api.resources.item import blp as ItemBlueprint
+from blog_api.resources.store import blp as StoreBlueprint
+from blog_api.resources.tag import blp as TagBlueprint
+from blog_api.blocklist import BLOCKLIST
 
 
-app = Flask(__name__)
+
 
 
 def create_app(db_url=None):
+    
+    app = Flask(__name__)
 
     app.config["PROPAGATE_EXCEPTIONS"] = True
     app.config["API_TITLE"] = "BLOP-REST-API"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/doc"
+    app.config["OPENAPI_REDOC_PATH"] = "/redoc"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
@@ -28,6 +33,14 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "tunga-impact-academy-2024"
 
     jwt = JWTManager(app)
+
+    db.init_app(app)
+
+    api = Api(app)
+
+    app.config["JWT_SECRET_KEY"] = "tunga-impact-academy-2024" # Use str(secrets.SystemRandom().getrandbits(128)) to generate a random secret key
+    jwt = JWTManager(app)
+    
 
     @jwt.additional_claims_loader
     def add_claims_to_jwt(identity):
@@ -77,15 +90,21 @@ def create_app(db_url=None):
             401,
         )
     
-    db.init_app(app)
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
+    
 
-    api = Api(app)
-
-    app.config["JWT_SECRET_KEY"] = "tunga-impact-academy-2024" # Use str(secrets.SystemRandom().getrandbits(128)) to generate a random secret key
-    jwt = JWTManager(app)
-
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
 
     api.register_blueprint(UserBlueprint)
     api.register_blueprint(ItemBlueprint)
