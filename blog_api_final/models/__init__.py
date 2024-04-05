7e31cff19a719248900c6638fe2e7bac426cca58
martin-creator <martinlubowa@outlook.com>
2024-04-05 16:07:06 +0300

    feat: create blog_api_final/models/__init__.py

---
+++ blog_api_final/models/__init__.py
@@ -0,0 +1,5 @@
+from models.user import UserModel
+from models.category import CategoryModel
+from models.author import AuthorModel
+from models.post import PostModel
+from models.post_categories import PostsCategories
