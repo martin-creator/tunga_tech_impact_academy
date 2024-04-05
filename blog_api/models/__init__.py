f8bd5f6fa44fd835c3c29d14765392fac0969f4e
martin-creator <martinlubowa@outlook.com>
2024-04-05 16:08:17 +0300

    chore: delete blog_api/models/__init__.py

--- blog_api/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from blog_api.models.user import UserModel
-from blog_api.models.item import ItemModel
-from blog_api.models.tag import TagModel
-from blog_api.models.store import StoreModel
-from blog_api.models.item_tags import ItemsTags
