59670be9770c27257e5b05c55ca4231cac4bb6aa
martin-creator <martinlubowa@outlook.com>
2024-04-05 11:14:24 +0300

    Updated store model

--- blog_api/models/store.py
+++ blog_api/models/store.py
@@ -8,6 +8,7 @@ class StoreModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     items = db.relationship("ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete")
+    tags = db.relationship("TagModel", back_populates="store", lazy="dynamic")
 
     # lazy="dynamic" is used to tell SQLAlchemy to not load the items automatically
     # This is useful when we have a large number of items in the database
