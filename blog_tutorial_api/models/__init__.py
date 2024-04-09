4454d03366e3345a974eef1b60aae1dbdde8bab5
martin-creator <martinlubowa@outlook.com>
2024-04-09 17:14:43 +0300

    feat: create blog_tutorial_api/models/__init__.py

---
+++ blog_tutorial_api/models/__init__.py
@@ -0,0 +1,5 @@
+from blog_api.models.user import UserModel
+from blog_api.models.item import ItemModel
+from blog_api.models.tag import TagModel
+from blog_api.models.store import StoreModel
+from blog_api.models.item_tags import ItemsTags
