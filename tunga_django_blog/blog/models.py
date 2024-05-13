41aa7bc183048d4c4bf8cd7bb479da815c4496b5
martin-creator <martinlubowa@outlook.com>
2024-05-13 21:04:48 +0300

    Added post models

--- tunga_django_blog/blog/models.py
+++ tunga_django_blog/blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
