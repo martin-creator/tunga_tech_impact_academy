02f849e9c1e66ef1470950d1f1253ee0bb71c141
martin-creator <martinlubowa@outlook.com>
2024-05-14 13:47:42 +0300

    update urls.py

--- tunga_django_blog/tunga_django_blog/urls.py
+++ tunga_django_blog/tunga_django_blog/urls.py
@@ -15,11 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
     path('admin/', admin.site.urls),
+
+    path('api/v1/', include('blog_django_api.urls')),
 
     path('', views.home),
     path('about/', views.about, name='about'),
