aa6575a96caf05126caead3a373b4ffd10bc0d8e
martin-creator <martinlubowa@outlook.com>
2024-05-14 19:31:43 +0300

    Updated models

--- tunga_django_blog/blog_django_api/views.py
+++ tunga_django_blog/blog_django_api/views.py
@@ -49,187 +49,6 @@ from drf_spectacular.types import OpenApiTypes
 #         responses={201: OpenApiResponse(response=OpenApiTypes.OBJECT, description='User data')}
 #     )
 
-
-class PostList(APIView):
-    """
-    List all posts, or create a new post.
-
-    """
-    permission_classes = [IsAuthenticated]
-
-    @extend_schema(
-        parameters=[
-            OpenApiParameter(name='title', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='content', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='author', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='status', type=str, location=OpenApiParameter.QUERY, required=True),
-        ],
-        examples=[
-            OpenApiExample(
-                'Example 1',
-                summary='Post Creation',
-                description='Create a new post',
-                value={
-                    "title": "title",
-                    "content": "content",
-                    "author": "author",
-                    "status": "status",
-                }
-            )
-        ],
-        responses={201: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
-    )
-
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-    @extend_schema(
-        responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
-    )
-    
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-    
-    @extend_schema(
-        responses={204: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
-    )
-    
-    def delete(self, request):
-        posts = Post.objects.all()
-        posts.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-    @extend_schema(
-        parameters=[
-            OpenApiParameter(name='title', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='content', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='author', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='status', type=str, location=OpenApiParameter.QUERY, required=True),
-        ],
-        examples=[
-            OpenApiExample(
-                'Example 1',
-                summary='Post Update',
-                description='Update a post',
-                value={
-                    "title": "title",
-                    "content": "content",
-                    "author": "author",
-                    "status": "status",
-                }
-            )
-        ],
-        responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
-    )
-    
-    def put(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, data=request.data, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-
-    
-
-class PostDetail(APIView):
-
-    """
-    Retrieve, update or delete a post instance.
-    
-    """
-    permission_classes = [IsAuthenticated]
-
-    @extend_schema(
-        parameters=[
-            OpenApiParameter(name='title', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='content', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='author', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='status', type=str, location=OpenApiParameter.QUERY, required=True),
-        ],
-        examples=[
-            OpenApiExample(
-                'Example 1',
-                summary='Post Update',
-                description='Update a post',
-                value={
-                    "title": "title",
-                    "content": "content",
-                    "author": "author",
-                    "status": "status",
-                }
-            )
-        ],
-        responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
-    )
-
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            Response(status=status.HTTP_404_NOT_FOUND)
-
-    @extend_schema(
-        responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
-    )
-
-    def get(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-    
-    @extend_schema(
-        parameters=[
-            OpenApiParameter(name='title', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='content', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='author', type=str, location=OpenApiParameter.QUERY, required=True),
-            OpenApiParameter(name='status', type=str, location=OpenApiParameter.QUERY, required=True),
-        ],
-        examples=[
-            OpenApiExample(
-                'Example 1',
-                summary='Post Update',
-                description='Update a post',
-                value={
-                    "title": "title",
-                    "content": "content",
-                    "author": "author",
-                    "status": "status",
-                }
-            )
-        ],
-        responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
-    )
-
-    def put(self, request, pk):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    @extend_schema(
-        responses={204: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
-    )
-
-    def delete(self, request, pk):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    
-# we use only use username', 'first_name', 'last_name', 'email', 'password' for usermosel
-
-
 class UserRegister(APIView):
     """
     Register a new user, or list all users, or delete all users.
@@ -298,6 +117,156 @@ class UserLogin(APIView):
             return Response({'token': token.key}, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
     
+
+# post model has these fields: 'title', 'description'
+class PostList(APIView):
+    """
+    List all posts, or create a new post.
+
+    """
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(name='title', type=str, location=OpenApiParameter.QUERY, required=True),
+            OpenApiParameter(name='description', type=str, location=OpenApiParameter.QUERY, required=True),
+        ],
+        examples=[
+            OpenApiExample(
+                'Example 1',
+                summary='Post Creation',
+                description='Create a new post',
+                value={
+                    "title": "title",
+                    "description": "description",
+                }
+            )
+        ],
+        responses={201: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
+    )
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    @extend_schema(
+        responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
+    )
+
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    
+    @extend_schema(
+        responses={204: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
+    )
+
+    def delete(self, request):
+        posts = Post.objects.all()
+        posts.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(name='title', type=str, location=OpenApiParameter.QUERY, required=True),
+            OpenApiParameter(name='description', type=str, location=OpenApiParameter.QUERY, required=True),
+        ],
+        examples=[
+            OpenApiExample(
+                'Example 1',
+                summary='Post Update',
+                description='Update a post',
+                value={
+                    "title": "title",
+                    "description": "description",
+                }
+            )
+        ],
+        responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
+    )
+
+    def put(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+    
+
+
+    
+
+class PostDetail(APIView):
+
+    """
+    Retrieve, update or delete a post instance.
+    
+    """
+    permission_classes = [IsAuthenticated]
+
+    @extend_schema(
+       parameters=[
+            OpenApiParameter(name='title', type=str, location=OpenApiParameter.QUERY, required=True),
+            OpenApiParameter(name='description', type=str, location=OpenApiParameter.QUERY, required=True),
+       ],
+         examples=[
+              OpenApiExample(
+                'Example 1',
+                summary='Post Update',
+                description='Update a post',
+                value={
+                     "title": "title",
+                     "description": "description",
+                }
+              )
+         ],
+
+            responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
+   )
+    
+    def put(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializer(post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    @extend_schema(
+        responses={204: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
+    )
+
+    def delete(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+    @extend_schema(
+        responses={200: OpenApiResponse(response=OpenApiTypes.OBJECT, description='Post data')}
+    )
+
+    def get(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+    
+
+
+    
+# we use only use username', 'first_name', 'last_name', 'email', 'password' for usermosel
+
+
+
    
     
 
