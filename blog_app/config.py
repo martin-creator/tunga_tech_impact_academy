b25cc293a49ea4bbc0e401ff81b78eb9a68a8e35
martin-creator <martinlubowa@outlook.com>
2024-05-23 16:11:45 +0300

    Added logic to deploy flask_blog

--- blog_app/config.py
+++ blog_app/config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)\
         or 'sqlite:///' + os.path.join(basedir, 'blog.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
